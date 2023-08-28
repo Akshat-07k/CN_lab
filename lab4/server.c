a34113bce6be82d6474cc941a562bb8489f90716
Akshat kumar <108222102+Akshat-07k@users.noreply.github.com>
2023-08-29 02:10:36 +0530

    Update server.c

--- lab4/server.c
+++ lab4/server.c
@@ -1,7 +1,3 @@
-// #include <stdio.h>
-// #include <sys/types.h>
-// #include <sys/socket.h>
-// #include <netinet.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,8 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
-// int socket(AF_INET, SOCK_DGRAM, 0);
-
 int main()
 {
 
@@ -24,34 +18,30 @@ int main()
 
     // BIND
     struct sockaddr_in sock;
-
-    sock.sin_family = AF_INET;
-    sock.sin_port = 1030;
-    sock.sin_addr.s_addr = inet_addr("10.5.77.175");
-
-    int bindInt = bind(cid, (struct sockaddr *)&sock, sizeof(sock));
-    if (bindInt == -1)
-    {
-        printf(" -------------Bind Uncessfull--------------\n ");
-    }
-
-    struct sockaddr_in from;
-    from.sin_family = AF_INET;
-    from.sin_port = 1040;
-    from.sin_addr.s_addr = inet_addr("10.5.77.175");
-
-    // char send[100]={"abcd"};
-    // scanf(" %s",&send);
-
-    // int bc = sendto(cid , send, sizeof(send), 0 , (struct sockaddr*)&to, sizeof(to));
-    int bc = bind(cid, (struct sockaddr *)&sock, sizeof(sock));
-    if (bc == -1 || bc == 0)
-    {
-        printf(" Fail ");
-        return 0;
-    }
+	sock.sin_family = AF_INET;
+	sock.sin_port = 1030;
+	sock.sin_addr.s_addr = inet_addr("10.0.2.15");
+	
+	int bindInt = bind(cid, (struct sockaddr *)&sock, sizeof(sock));
+	    if (bindInt == -1)
+	    {
+		printf(" -------------Bind Uncessfull--------------\n ");
+		return 0;
+	    }
+	    
+	  struct sockaddr_in from;
+	  char data[100];
+	  int recv,k;
+	 recv = recvfrom(cid , data , 100, 0 , (struct sockaddr*)&from , &k);
+	 if(recv==-1)
+	 {
+	 	printf(" Error on recv ");
+	 	return 0;
+	 }
+	 printf(" %s",data);
+	 
 
     // close(cid);
-    printf(" Khudos Client  is ready ");
+    printf(" Khudos SERVER  is ready ");
     return 0;
 }
